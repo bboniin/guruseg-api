07a926e2bd3b0475cd99c9caa3ac90129deb106d
José Bonifácio <boninho7834@gmail.com>
2024-09-26 14:50:24 -0300

    teste build

--- src/controllers/Lead/CreateLeadWebController.ts
+++ src/controllers/Lead/CreateLeadWebController.ts
@@ -5,6 +5,8 @@ class CreateLeadWebController {
     async handle(req: Request, res: Response) {
         const { name, observation, value, email, location, cnpj, necessity, phone_number, employees, send } = req.body
 
+        console.log(req.body)
+
         if(req.headers.authorization != "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVGVzdGUiLCJlbWFpbCI6InRlc3RlIiwiaWF0IjoxN"){
             throw new Error("Token inválido")
         }
