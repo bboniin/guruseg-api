026db381cae3b20f12790179c01b43e247a1be6c
José Bonifácio <boninho7834@gmail.com>
2024-10-25 19:38:12 -0300

    teste 5

--- src/services/Admin/Banners/ListBannersPublicService.ts
+++ src/services/Admin/Banners/ListBannersPublicService.ts
@@ -19,34 +19,6 @@ class ListBannersPublicService {
             }
         })
 
-        const contracts = await prismaClient.contract.findMany({
-            where: {
-                lead_id: {
-                    contains: "-"
-                }
-            }
-        })
-
-        Promise.all(
-            await contracts.map(async (item)=>{
-                const lead = await prismaClient.lead.findFirst({
-                    where: {
-                        id: item.lead_id
-                    }
-                })
-                if(!lead){
-                    await prismaClient.contract.update({
-                        where: {
-                            id: item.id
-                        },
-                        data: {
-                            lead_id: null
-                        }
-                    })
-                }
-            })
-        )
-
         return (banners)
     }
 }
