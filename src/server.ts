5eedeb8ece7f440c0bdbf1faa9429a68581d84a6
José Bonifácio <boninho7834@gmail.com>
2024-10-26 01:07:40 -0300

    ajuste final

--- src/server.ts
+++ src/server.ts
@@ -53,4 +53,4 @@ cron.schedule("0 8 * * *", () => {
 });
 
 
-app.listen(3333, () => console.log("rodando v49"))
+app.listen(3333, () => console.log("rodando v50"))
--- src/services/Admin/Banners/ListBannersPublicService.ts
+++ src/services/Admin/Banners/ListBannersPublicService.ts
@@ -18,38 +18,6 @@ class ListBannersPublicService {
                 url: true
             }
         })
-        
-        const contracts = await prismaClient.contract.findMany()
-
-        Promise.all(
-            await contracts.map(async (item)=>{
-                const lead = await prismaClient.lead.findFirst({
-                    where: {
-                        id: item.lead_id
-                    }
-                })
-                if(!lead){
-                    await prismaClient.contract.update({
-                        where: {
-                            id: item.id
-                        },
-                        data: {
-                            lead_id: null
-                        }
-                    })
-                }
-                if(!item.lead_id){
-                    await prismaClient.contract.update({
-                        where: {
-                            id: item.id
-                        },
-                        data: {
-                            lead_id: null
-                        }
-                    })
-                }
-            })
-        )
 
         return (banners)
     }
