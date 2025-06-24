fb3e7b085c0ecbf0e826b1aaeca1c395c3c20c0b
José Bonifácio <boninho7834@gmai.com>
2025-06-24 13:01:19 -0300

    ajust overdue

--- src/services/Payment/ConfirmPaymentService.ts
+++ src/services/Payment/ConfirmPaymentService.ts
@@ -113,39 +113,41 @@ class ConfirmPaymentService {
           },
         });
 
-        await prismaClient.order.update({
-          where: {
-            id: paymentCanceled.order_id,
-          },
-          data: {
-            payment_id: null,
-            status_payment: "expirado",
-          },
-        });
+        if (order) {
+          await prismaClient.order.update({
+            where: {
+              id: paymentCanceled.order_id,
+            },
+            data: {
+              payment_id: null,
+              status_payment: "expirado",
+            },
+          });
 
-        const path = resolve(
-          __dirname,
-          "..",
-          "..",
-          "views",
-          "expiredPayment.hbs"
-        );
+          const path = resolve(
+            __dirname,
+            "..",
+            "..",
+            "views",
+            "expiredPayment.hbs"
+          );
 
-        const templateFileContent = fs.readFileSync(path).toString("utf-8");
+          const templateFileContent = fs.readFileSync(path).toString("utf-8");
 
-        const templateParse = handlebars.compile(templateFileContent);
+          const templateParse = handlebars.compile(templateFileContent);
 
-        const templateHTML = templateParse({
-          name: order.user.name,
-          order_id: order.id,
-        });
+          const templateHTML = templateParse({
+            name: order.user.name,
+            order_id: order.id,
+          });
 
-        await resend.emails.send({
-          from: "Equipe Guruseg <noreply@gurusegead.com.br>",
-          to: order.user.email,
-          subject: "[Guruseg] Pagamento Expirado",
-          html: templateHTML,
-        });
+          await resend.emails.send({
+            from: "Equipe Guruseg <noreply@gurusegead.com.br>",
+            to: order.user.email,
+            subject: "[Guruseg] Pagamento Expirado",
+            html: templateHTML,
+          });
+        }
       }
 
       if (data["event"] == "PAYMENT_DELETED") {
@@ -158,39 +160,41 @@ class ConfirmPaymentService {
           },
         });
 
-        await prismaClient.order.update({
-          where: {
-            id: paymentCanceled.order_id,
-          },
-          data: {
-            payment_id: null,
-            status_payment: "cancelado",
-          },
-        });
+        if (order) {
+          await prismaClient.order.update({
+            where: {
+              id: paymentCanceled.order_id,
+            },
+            data: {
+              payment_id: null,
+              status_payment: "cancelado",
+            },
+          });
 
-        const path = resolve(
-          __dirname,
-          "..",
-          "..",
-          "views",
-          "canceledPayment.hbs"
-        );
+          const path = resolve(
+            __dirname,
+            "..",
+            "..",
+            "views",
+            "canceledPayment.hbs"
+          );
 
-        const templateFileContent = fs.readFileSync(path).toString("utf-8");
+          const templateFileContent = fs.readFileSync(path).toString("utf-8");
 
-        const templateParse = handlebars.compile(templateFileContent);
+          const templateParse = handlebars.compile(templateFileContent);
 
-        const templateHTML = templateParse({
-          name: order.user.name,
-          order_id: order.id,
-        });
+          const templateHTML = templateParse({
+            name: order.user.name,
+            order_id: order.id,
+          });
 
-        await resend.emails.send({
-          from: "Equipe Guruseg <noreply@gurusegead.com.br>",
-          to: order.user.email,
-          subject: "[Guruseg] Pagamento Cancelado",
-          html: templateHTML,
-        });
+          await resend.emails.send({
+            from: "Equipe Guruseg <noreply@gurusegead.com.br>",
+            to: order.user.email,
+            subject: "[Guruseg] Pagamento Cancelado",
+            html: templateHTML,
+          });
+        }
       }
     }
     return data;
